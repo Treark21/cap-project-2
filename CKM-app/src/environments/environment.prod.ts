ab9e5b98ea86b85c7fa4426914f78a156f21b2da
Treark21 <58716148+Treark21@users.noreply.github.com>
2020-02-22 19:05:49 -0600

    Update environment.prod.ts

--- CKM-app/src/environments/environment.prod.ts
+++ CKM-app/src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyC3NSBqNSXW_9DwjLCw9uEmibc5jateCaQ",
-    authDomain: "cap-proj-practice.firebaseapp.com",
-    databaseURL: "https://cap-proj-practice.firebaseio.com",
-    projectId: "cap-proj-practice",
-    storageBucket: "cap-proj-practice.appspot.com",
-    messagingSenderId: "1005077049627",
-    appId: "1:1005077049627:web:6f34e24009a23e1ce8a3be",
-    measurementId: "G-4C2X5XM339"
+
   }
 };
